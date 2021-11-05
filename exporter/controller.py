ae58663b56815e6ff5e3e2cc86ab7add149bbecb
TitomNCL <70750510+titomncl@users.noreply.github.com>
2021-11-05 10:30:11 +0100

    🐛 fix error with compute export path (#11)

--- exporter/controller.py
+++ exporter/controller.py
@@ -11,14 +11,13 @@ class Controller(object):
 
     def export_asset(self, export_hd, only_hd):
         if not only_hd:
-            self.core.export_abc(self.core.compute_export_path("LD"))
+            self.core.export_abc(self.core().compute_export_path("LD"))
 
         if only_hd or export_hd:
             self.core.compute_hd_export()
 
     def export_anim(self, start, end):
-        self.core.export_abc(self.core.compute_export_path(), start, end)
+        self.core.export_abc(self.core().compute_export_path(), start, end)
 
     def export_camera(self, start, end, comment):
-        print(self.core.compute_export_path(comment))
-        self.core.export_abc(self.core.compute_export_path(comment), start, end)
+        self.core.export_abc(self.core().compute_export_path(comment), start, end)
